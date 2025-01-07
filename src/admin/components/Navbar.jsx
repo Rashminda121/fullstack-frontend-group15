341d6bd3f3b0ec2f04f97173bf622ef7b6d1fac6
Jayamuni Rashminda <rashminda121@gmail.com>
2025-01-07 23:48:47 +0530

    updates

--- src/admin/components/Navbar.jsx
+++ src/admin/components/Navbar.jsx
@@ -23,7 +23,7 @@ const NavbarAdmin = () => {
 
   const menuItems = [
     { name: "Home", path: "/admin" },
-    { name: "Items", path: "/src/admin/pages/Items.jsx" },
+    { name: "Items", path: "/admin/itemsPage" },
     { name: "Categories", path: "/admin/categoriesPage" },
     { name: "Orders", path: "/admin/ordersPage" },
     { name: "Offers", path: "/admin/offersPage" },
